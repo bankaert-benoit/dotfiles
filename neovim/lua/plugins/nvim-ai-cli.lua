f933a4b7a9c402f2241abcfac686f4ee40e69170
Bankaert Benoit <bankaert.benoit@gmail.com>
2026-06-18 12:49:07 +0200

    feat(nvim): update nvim-ai-cli plugin

--- neovim/lua/plugins/nvim-ai-cli.lua
+++ neovim/lua/plugins/nvim-ai-cli.lua
@@ -1,23 +1,9 @@
 return {
-	{ 
-		dir = "~/perso/nvim-ai-cli", 
-		config = function()
-			require("nvim-ai-cli").setup({
-				cmd = "agy",
-				on_open = function()
-					vim.notify("Test on open")
-				end,
-				on_close = function()
-					vim.notify("Test on close")
-				end,
-			})
-		end,
-	}
-	-- "bankaert-benoit/nvim-ai-cli",
-	-- tag = "v0.1",
-	--  config = function()
-	--    require("nvim-ai-cli").setup({
-	--      cmd = "agy",
-	--    })
-	--  end,
+	"bankaert-benoit/nvim-ai-cli",
+	tag = "v0.1",
+	 config = function()
+	   require("nvim-ai-cli").setup({
+	     cmd = "agy",
+	   })
+	 end,
 } 
